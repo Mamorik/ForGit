86fce4199059fcd460f95918945508e420de90fd
Your Name <you@example.com>
2020-11-09 13:02:38 +0300

    TestPass

--- ForGit/Program.cs
+++ ForGit/Program.cs
@@ -8,5 +8,22 @@ namespace ForGit
         {
             Console.WriteLine("Hello World!");
         }
+
+        public  bool PassCheck(string pass)
+        {
+            if (pass.Length < 7 && pass.Length > 13)
+            {
+                return false;
+            } else
+            {
+                for (int i = 0; i < pass.Length; i++)
+                    if (char.IsUpper(pass[i]))
+                    {
+                        return true;
+                    }
+            }
+            return false;
+
+        }
     }
 }
